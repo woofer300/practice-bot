5ef59a49822a4744ad28b0168c809b8d2fdd05fe
lahren1 <97566352+lahren1@users.noreply.github.com>
2022-01-26 17:52:34 -0800

    changed configure turret

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.StatusFrame;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -17,52 +19,47 @@ public class TurretSubsystem extends SubsystemBase {
 
     public void configureTurret() {
         
-        //Factory Default to ensure that talon behaves the same all the time
-        m_turretMotor.configFactoryDefault();
+    /* Factory default hardware to prevent unexpected behavior */
+    m_turretMotor.configFactoryDefault();
 
-        //Zero the encoder
-		m_turretMotor.setSelectedSensorPosition(0, TurretConstants.kPIDLoopIdx, TurretConstants.kTimeoutMs);
+    /* Configure Sensor Source for Pirmary PID */
+    m_turretMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, TurretConstants.kPIDLoopIdx, TurretConstants.kTimeoutMs);
 
-        //Configure sensor source for primary PID
-        m_turretMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, TurretConstants.kPIDLoopIdx, TurretConstants.kTimeoutMs);
+    /* set deadband to super small 0.001 (0.1 %).
+    The default deadband is 0.04 (4 %) */
+    m_turretMotor.configNeutralDeadband(0.001, TurretConstants.kTimeoutMs);
 
-        //Set the deadband to a super small value
-        m_turretMotor.configNeutralDeadband(0.001, TurretConstants.kTimeoutMs);
+    /**
+     * Configure Talon SRX Output and Sensor direction accordingly Invert Motor to
+     * have green LEDs when driving Talon Forward / Requesting Postiive Output Phase
+     * sensor to have positive increment when driving Talon Forward (Green LED)
+     */
+    m_turretMotor.setSensorPhase(true);
+    m_turretMotor.setInverted(false);
 
-        /**
-		 * Configure Talon SRX Output and Sensor direction accordingly Invert Motor to
-		 * have green LEDs when driving Talon Forward / Requesting Postiive Output Phase
-		 * sensor to have positive increment when driving Talon Forward (Green LED)
-		 */
-        m_turretMotor.setSensorPhase(true);
-        m_turretMotor.setInverted(false);
-        
-        //Set relative Frame periods to be at least as fast as periodic rate
-        m_turretMotor.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 10, TurretConstants.kTimeoutMs);
-        m_turretMotor.setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 10, TurretConstants.kTimeoutMs);
+    /* Set relevant frame periods to be at least as fast as periodic rate */
+    m_turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, TurretConstants.kTimeoutMs);
+    m_turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, TurretConstants.kTimeoutMs);
 
-        //Set the peak and nominal outputs
-        m_turretMotor.configNominalOutputForward(0, TurretConstants.kTimeoutMs);
-		m_turretMotor.configNominalOutputReverse(0, TurretConstants.kTimeoutMs);
-		m_turretMotor.configPeakOutputForward(1, TurretConstants.kTimeoutMs);
-		m_turretMotor.configPeakOutputReverse(-1, TurretConstants.kTimeoutMs);
+    /* Set the peak and nominal outputs */
+    m_turretMotor.configNominalOutputForward(0, TurretConstants.kTimeoutMs);
+    m_turretMotor.configNominalOutputReverse(0, TurretConstants.kTimeoutMs);
+    m_turretMotor.configPeakOutputForward(1, TurretConstants.kTimeoutMs);
+    m_turretMotor.configPeakOutputReverse(-1, TurretConstants.kTimeoutMs);
 
-        //Set the motion magic in slot 0
-        m_turretMotor.selectProfileSlot(TurretConstants.kSlotIdx, TurretConstants.kPIDLoopIdx);
-		m_turretMotor.config_kF(TurretConstants.kSlotIdx, TurretConstants.kF, TurretConstants.kTimeoutMs);
-		m_turretMotor.config_kP(TurretConstants.kSlotIdx, TurretConstants.kP, TurretConstants.kTimeoutMs);
-		m_turretMotor.config_kI(TurretConstants.kSlotIdx, TurretConstants.kI, TurretConstants.kTimeoutMs);
-		m_turretMotor.config_kD(TurretConstants.kSlotIdx, TurretConstants.kD, TurretConstants.kTimeoutMs);
+    /* Set Motion Magic gains in slot0 - see documentation */
+    m_turretMotor.selectProfileSlot(TurretConstants.kSlotIdx, TurretConstants.kPIDLoopIdx);
+    m_turretMotor.config_kF(TurretConstants.kSlotIdx, TurretConstants.kF, TurretConstants.kTimeoutMs);
+    m_turretMotor.config_kP(TurretConstants.kSlotIdx, TurretConstants.kP, TurretConstants.kTimeoutMs);
+    m_turretMotor.config_kI(TurretConstants.kSlotIdx, TurretConstants.kI, TurretConstants.kTimeoutMs);
+    m_turretMotor.config_kD(TurretConstants.kSlotIdx, TurretConstants.kD, TurretConstants.kTimeoutMs);
 
-        //Set the max cruise velocity and acceleration
-        m_turretMotor.configMotionCruiseVelocity(TurretConstants.MAX_VELOCITY, TurretConstants.kTimeoutMs);
-        m_turretMotor.configMotionCruiseVelocity(TurretConstants.MAX_ACCELERATION, TurretConstants.kTimeoutMs);
+    /* Set acceleration and vcruise velocity - see documentation */
+    m_turretMotor.configMotionCruiseVelocity(3000, TurretConstants.kTimeoutMs);
+    m_turretMotor.configMotionAcceleration(3000, TurretConstants.kTimeoutMs);
 
-        //Sets peak current
-        m_turretMotor.configPeakCurrentLimit(20);
-
-        //The allowable error 
-        m_turretMotor.configAllowableClosedloopError(TurretConstants.kSlotIdx, 10, TurretConstants.kTimeoutMs);
+    /* Zero the sensor once on robot boot up */
+    m_turretMotor.setSelectedSensorPosition(0, TurretConstants.kPIDLoopIdx, TurretConstants.kTimeoutMs);
 
     }
 
