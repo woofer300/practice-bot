4422fda7d43afe91540202247def1d155876eeff
Chris Voon <christopher.voon@gmail.com>
2022-01-28 16:57:06 -0800

    This should work

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,6 +4,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
@@ -14,15 +15,29 @@ public class IntakeSubsystem extends SubsystemBase {
     private final DoubleSolenoid rightDoubleSolenoid;
 
     public IntakeSubsystem() {
-        leftDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, 0, 1);
+        leftDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.LEFT_PNEUMATIC_FORWARD, IntakeConstants.LEFT_PNEUMATIC_REVERSE);
         leftDoubleSolenoid.set(DoubleSolenoid.Value.kOff);
-        rightDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, 6, 7);
+        rightDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.RIGHT_PNEUMATIC_FORWARD, IntakeConstants.RIGHT_PNEUMATIC_REVERSE);
         rightDoubleSolenoid.set(DoubleSolenoid.Value.kOff);
     }
 
     @Override
     public void periodic() {
-        
+        SmartDashboard.putString("Left Pneumatic State", solenoidStatus(leftDoubleSolenoid));
+        SmartDashboard.putString("Left Pneumatic State", solenoidStatus(rightDoubleSolenoid));
+    }
+
+    private String solenoidStatus(DoubleSolenoid solenoid) {
+        DoubleSolenoid.Value solenoidState = solenoid.get();
+        if (solenoidState.equals(DoubleSolenoid.Value.kForward)) {
+            return "Forward";
+        } else if (solenoidState.equals(DoubleSolenoid.Value.kReverse)) {
+            return "Reverse";
+        } else if (solenoidState.equals(DoubleSolenoid.Value.kOff)) {
+            return "Off";
+        } else {
+            return "Error";
+        }
     }
     
     public void intake() {
@@ -32,11 +47,13 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void extend() {
-        doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+        leftDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+        rightDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
     }
 
     public void retract() {
-        doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
 }
 
